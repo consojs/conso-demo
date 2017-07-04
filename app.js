bcab24f96291fb0d3c06d2e6a0e47f2770d4bdeb
tagee <896932646@qq.com>
2017-07-04 23:49:20 +0800

    test：集成context，修改json，jsonp，redirect函数bug。

--- app.js
+++ app.js
@@ -1,3 +1,2 @@
 let {Application} = require('conso');
-new Application()
-    .run();
+new Application().run();
--- controller/index.js
+++ controller/index.js
@@ -4,25 +4,27 @@ let User = require('../module/User');
 let {route, get, post, resource} = Annotation;
 
 @route('/home')
-class Index {
+class index {
 
     @resource(User, '张三')
     user;
 
     @get('/test1')
-    test1(req, res, next) {
+    test1(ctx, next) {
         console.log(this.user.name);
-        res.render('index', {title: 'Conso'});
+        // ctx.render('index', {title: 'conso'});
+        ctx.end('qweqwe')
+        ctx.redirect('https://baidu.com');
     }
 
     @get('/test2')
-    test2(req, res, next) {
-        res.json({username: this.user.name});
+    test2(ctx, next) {
+        ctx.jsonp({username: this.user.name});
     }
 
     @get('/test3')
-    test3(req, res, next) {
+    test3(ctx, next) {
         this.user.name = 'lisi';
-        res.json({username: this.user.name});
+        ctx.json({username: this.user.name});
     }
 }
